a720c4e69a10708ba060b28c4ce8e73b8c5a1956
Ruth Johnston <ruthjohnstoncodes@gmail.com>
2018-03-26 17:50:52 +0100

    4.2 changed price datatype to decimal

--- db/migrate/20180323163214_add_color_to_products.rb
+++ db/migrate/20180323163214_add_color_to_products.rb
@@ -1,5 +1,5 @@
 class AddColorToProducts < ActiveRecord::Migration[5.1]
   def change
-    add_column :products, :color, :decimal
+    add_column :products, :color, :string
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180326134412) do
+ActiveRecord::Schema.define(version: 20180326164620) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 20180326134412) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "area"
-    t.string "price"
+    t.decimal "price"
   end
 
 end
