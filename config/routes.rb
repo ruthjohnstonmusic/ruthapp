2f4d2bd7e9f8e4697e64d83c6881ddc8fe08b828
Ruth Johnston <ruthjohnstoncodes@gmail.com>
2018-05-07 13:59:49 +0100

    5.1.1 added landing_page to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   get 'simple_pages/contact'
 
   get 'simple_pages/index'
+
+  get 'simple_pages/landing_page'
   root 'simple_pages#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
