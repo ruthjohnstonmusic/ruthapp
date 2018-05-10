3c4493525652e650a2592c8fcd94bd1d30d68d63
Ruth Johnston <ruthjohnstoncodes@gmail.com>
2018-05-10 15:43:41 +0100

    5.3 corrected email address in simple_pages controller

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -12,7 +12,7 @@ class SimplePagesController < ApplicationController
     @email = params[:email]
     @message = params[:message]
     ActionMailer::Base.mail(from: @email,
-        to: 'ruthjohnstoncodes@example.com',
+        to: 'ruthjohnstoncodes@gmail.com',
         subject: "A new contact form message from #{@name}",
         body: @message).deliver_now
   end
