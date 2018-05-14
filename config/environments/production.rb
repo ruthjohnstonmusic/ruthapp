61a4b46db98dc166dffbe8dbd8a90a69022d886f
Ruth Johnston <ruthjohnstoncodes@gmail.com>
2018-05-14 13:25:22 +0100

    5.4 user db created, devise gem added, login added and styled

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,9 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  ## dynamic links that show up inside emails from production environment generate the right URLs
+  config.action_mailer.default_url_options = { host: ‘laurel-cottage-ltd.herokuapp.com’ }
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout' }
+  resources :users
   resources :orders, only: [:index, :show, :create, :destroy]
   resources :products
   get 'simple_pages/about'
@@ -10,7 +12,7 @@ Rails.application.routes.draw do
   get 'simple_pages/landing_page'
 
   post 'simple_pages/thank_you'
-  
+
   root 'simple_pages#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
