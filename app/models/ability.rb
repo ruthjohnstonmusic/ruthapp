16ead290cdc153965e271b36573bb653eaa1eb2c
Ruth Johnston <ruthjohnstoncodes@gmail.com>
2018-08-09 10:54:55 +0100

    5.10.3 amended ability class

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,7 +3,7 @@ class Ability
 
   def initialize(user)
     user ||= User.new # guest user (not logged in)
-    can :read, :all 
+    can :read, :all .
     if user.admin?
       can :manage, :all
     else
