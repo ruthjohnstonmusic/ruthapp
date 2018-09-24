677ee41f918473668057bc62a354cf03761acc8e
Ruth Johnston <ruthjohnstoncodes@gmail.com>
2018-09-24 16:16:38 +0100

    6.7.1 removed byebug line from products_controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -19,7 +19,6 @@ class ProductsController < ApplicationController
   # GET /products/1
   # GET /products/1.json
   def show
-    byebug
     @comments = @product.comments.order("created_at DESC").page(params[:page]).per_page(3)
   end
 
